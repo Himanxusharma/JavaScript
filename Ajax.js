97417d7ecb6cea1cc47239f89d80d26227e8528d
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-06-07 10:53:09 +0530

    commit

--- Ajax.js
+++ Ajax.js
@@ -402,11 +402,45 @@
 // })
 
 
-
-
-
-
 // // promises
+// // why promises are used - Promises are used to handle asynchronous operations in JavaScript. They are easy to manage when dealing with multiple asynchronous operations where callbacks can create callback hell leading to unmanageable code.
+
+// //  promises produce
+// const probj1 = new Promise((resolve,reject)=>{
+
+//     setTimeout(()=>{
+//         let roll = [1,2,3,4,5,6];
+//           resolve(roll);
+//         // reject('Error happened');
+//     },1000);
+
+// });
+
+// const getdata =(indexdata)=>{
+//     return new Promise((resolve,reject)=>{
+//         setTimeout((indexdata) => {
+//             let biodata ={
+//                 name:'Sunny Deol',
+//                 age : 23
+
+//             }
+//             resolve(`My roll no is ${indexdata}. My name is ${biodata.name} and I am ${biodata.age} year old. `)
+//         }, 1000,indexdata);
+//     });
+// }
+
+// // .. promises consume
+// probj1.then((rollno)=>{
+//     console.log(rollno);
+//     return getdata(rollno[1])
+// }).then((data)=>{
+//     console.log(data);
+// }).catch((error)=>{
+//     console.log(error);
+// })
+
+
+
 
 // // async await
 
